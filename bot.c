45c112e515490deba12d7a90c8853b22132ca546
Rishab <senthir1@uci.edu>
2026-05-13 20:54:08 -0700

    Minor changes.

--- bot.c
+++ bot.c
@@ -3,15 +3,20 @@
 
 double monteCarloSim(const GameState* gs, int simCount)
 {
+    (void) gs;
+    (void) simCount;
     return 0.0;
 }
 
 MoveType decide(const GameState* gs, int socket)
 {
+    (void) gs;
+    (void) socket;
     return CHECK;
 }
 
 void checkRemainingCards(const GameState* gs)
 {
+    (void) gs;
     return;
 }
