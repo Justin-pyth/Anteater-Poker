66620ec81b45784572e3cfaaf3369a509b69f6c8
Rishab <senthir1@uci.edu>
2026-05-23 18:15:00 -0700

    Idea: ready if /ready in chat

--- gui.h
+++ gui.h
@@ -10,15 +10,18 @@
 
 
 //connect the send chat button and decide what to do
-void on_send(GtkEntry *entry, gpointer data);
+void on_send_chat_button_clicked(GtkEntry *entry, gpointer data);
 
 //call within on_send
-//create a message payload and send to server
+//prepare message payload and send to server
 void sendChatToServer(uint8_t sender_id, const char *chatMessage);
 
 //in the chat_log, append the chat message
 //to be called after server broadcasts new message to all clients
 void appendChat(const char *sender, const char *chatMessage);
+
+//call if user types /ready in chat
+void sendReadyToServer();
 
 
 #endif
