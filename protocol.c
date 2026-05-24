ffe3916651453703dde44220382387af63132594
duc tran <ductt2@uci.edu>
2026-05-23 18:31:30 -0700

    Change message type check from MSG_TYPE_READER to MSG_TYPE_READY

--- protocol.c
+++ protocol.c
@@ -62,7 +62,7 @@ void handle_client_communication(ServerState *state, Client *client)
             
             broadcast_chat_message(state, data.sender_id, data.chat);
         }
-           else if (data.type == MSG_TYPE_READER)
+           else if (data.type == MSG_TYPE_READY)
         {
             
         //implement game logic here
